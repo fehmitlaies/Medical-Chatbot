3920c27bb757ba5a0998fab60b863fc942719034
fehmitlaies <57046918+fehmitlaies@users.noreply.github.com>
2022-10-09 18:30:20 +0200

    Create run.py

---
+++ run.py
@@ -0,0 +1,14 @@
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""runner"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import optimization
+import run_classifier
+import tokenization
+import tensorflow as tf
+import tensorflow_hub as hub
